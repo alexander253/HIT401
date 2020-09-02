87ce2f1875e96d92e6a66b95bd945276b08ef46f
Alex <coolazn818@hotmail.com>
2020-09-02 23:54:06 +0930

    test

--- lib/sql/load.sql
+++ lib/sql/load.sql
@@ -4,7 +4,7 @@ use waste_app;
 INSERT INTO `user` (`email`, `fname`, `lname`, `points`, `hashed_password`, `salt`)
 VALUES
   ('bob@hotmail.com', 'Bob', 'Marley', '12', '1234', '1234'),
-  ('frank@gmail.com', 'Frank', 'Citezen', '23','1234','1234' ),
+  ('frank@gmail.com', 'Charles', 'Yeo', '23','1234','1234' ),
   ('silin@gmail.com', 'Silin', 'Chen','15', '1234','1234' ),
   ('james@gmail.com', 'James', 'Lin', '4','1234','1234' ),
   ('alex@gmail.com', 'Alex', 'Lay', '2', '1234','1234' )
